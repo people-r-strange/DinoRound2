5a6e2d58818cad5a3156289465c458cb26de173a
Mayeline Pena <41595678+maypena@users.noreply.github.com>
2021-03-15 23:33:34 -0400

    Adding Isabel and May's work

---
+++ May-Code/groups.py
@@ -0,0 +1,10 @@
+# Mayeline Pena
+# This code parses through the csv file to try to find groups 
+
+# read file
+# check who this ID communicated with 
+# check who that ID comminicated with 
+# put them all in a group
+
+#use pandas
+#https://realpython.com/python-csv/
